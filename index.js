738c5abe5c56a8e4ea6eadf7fb281a67a486dfeb
AnhQuann <anhquan19051905@gmail.com>
2019-01-25 15:47:44 +0700

    Function part 1

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-var rad = function(x) {
-    return x * Math.PI / 180;
-  };
-
-var getDistance = function(p1, p2) {
-var R = 6378137; // Earth’s mean radius in meter
-var dLat = rad(p2.lat() - p1.lat());
-var dLong = rad(p2.lng() - p1.lng());
-var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
-    Math.sin(dLong / 2) * Math.sin(dLong / 2);
-var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-var d = R * c;
-return d; // returns the distance in meter
-};
-
-console.log(getDistance(39.46, 40.40));
+for (let index = 1; index <= 3; index++) {
+  let number${index} = prompt("Input your number: ");
+      
+      if (number3 % 2 === 0){
+          console.log('Even Number');
+      } else {
+          console.log('Odd Number');
+      }
+  
+      muNumber.push(number3);
+}
